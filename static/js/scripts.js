47d0f09565a016eb9ef35c83ae66c565548c9cd3
luiz_mn <luizmontenegroneto@gmail.com>
2018-09-14 15:53:42 -0400

    working in jsfiddle

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,57 @@
+
+// In the following example, markers appear when the user clicks on the map.
+// The markers are stored in an array.
+// The user can then click an option to hide, show or delete the markers.
+var map;
+var markers = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+                              zoom: 12,
+                              center: {lat: 45.5016889, lng: -73.567256}
+                              });
+
+    setMarkers(map);
+}
+
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
+var locations = [
+                 ['The Montreal Museum of Fine Arts', 45.498522, -73.5794],
+                 ['Mount Royal Park', 45.504798, -73.587842],
+                 ['Ecole de technologie superiure ETS', 45.494546, -73.562246]
+                 ];
+
+function setMarkers(map) {
+    // Adds markers to the map.
+    for (var i = 0; i < locations.length; i++) {
+        var position = locations[i];
+        var marker = new google.maps.Marker({
+                                            position: {lat: position[1], lng: position[2]},
+                                            map: map,
+                                            title: position[0]
+                                            });
+        markers.push(marker);
+    }
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(markers);
+}
+
 //Function used to show distances
 var DistanceVM = function DistancesViewModel() {
     this.distances = [
@@ -24,62 +78,3 @@ var ModeVM = function TravelModesViewModel() {
 //The bindings are applied directly to the elements
 ko.applyBindings(DistanceVM, document.getElementById('selectDistance'));
 ko.applyBindings(ModeVM, document.getElementById('selectMode'));
-
-var map;
-
-var markers = [];
-
-var locations = [
-    { title: 'The Montreal Museum of Fine Arts', location: { lat: 45.498522, lng: -73.5794 } },
-    { title: 'Mount Royal Park', location: { lat: 45.504798, lng: -73.587842 } },
-    { title: 'Ecole de technologie superiure ETS', location: { lat: 45.494546, lng: -73.562246 } }
-];
-
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 45.5016889, lng: -73.567256 },
-        zoom: 13,
-        mapTypeId: 'roadmap'
-    });
-
-
-    // Create the markers based on locations array
-    for (var i = 0; i < locations.length; i++) {
-        // Get the position from the location array.
-        var position = locations[i].location;
-        var title = locations[i].title;
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE,
-                scale: 10
-            },
-            id: i
-        });
-        // Push the marker to our array of markers.
-        markers.push(marker);
-    }
-
-}
-// Function used to display all pre-selected markers
-function showListings() {
-    var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
-    }
-    map.fitBounds(bounds);
-}
-
-// Function used to hide all pre-selected markers
-function hideMarkers(markers) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-}
